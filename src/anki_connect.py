b918ffaff5a8ae2245074e71309bb3f1d8a812c4
Covey Afolami <coveyafolami@gmail.com>
2026-08-19 17:16:45 +0100

    Remove debug print statement for API key loading

--- src/anki_connect.py
+++ src/anki_connect.py
@@ -6,8 +6,6 @@ load_dotenv()
 
 anki_api_key = os.getenv("anki_api_key")
 
-print("API key loaded:", anki_api_key is not None)
-
 def get_deck_names():
     request = build_deck_names_request()
     response = send_request(request)
