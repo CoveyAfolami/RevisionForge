ca33a182329bd46d06fe145b85a0934213732a23
Covey Afolami <coveyafolami@gmail.com>
2026-08-19 17:20:18 +0100

    Fix get_deck_names to return an empty list by default and add print statements for deck names in main function

--- src/anki_connect.py
+++ src/anki_connect.py
@@ -9,7 +9,7 @@ anki_api_key = os.getenv("anki_api_key")
 def get_deck_names():
     request = build_deck_names_request()
     response = send_request(request)
-    return response.get("result", [])
+    return response["result"]
 
 def build_deck_names_request():
     return {
--- src/main.py
+++ src/main.py
@@ -5,11 +5,14 @@ def main():
 
     deck_names = ac.get_deck_names()
 
+    for deck_name in deck_names:
+        print(deck_name)
+
     request = ac.build_deck_names_request()
     #print("Request:", request)
 
     response = ac.send_request(request)
-    print(response)
+    #print(response)
 
 if __name__ == "__main__":
     main()
