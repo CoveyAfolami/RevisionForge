94d7f234cd3483fb3633b4157265c3b63a4aafd8
Covey Afolami <coveyafolami@gmail.com>
2026-08-20 12:53:54 +0100

    Refactor project structure: move anki_connect.py to anki directory, create lifecycle.py, and update main.py to utilize new module organization

---
+++ src/app/lifecycle.py
@@ -0,0 +1,2 @@
+def start_application():
+    print("Application started.")
---
+++ src/app/main.py
@@ -0,0 +1,25 @@
+from lifecycle import start_application
+import anki.anki_connect as ac
+
+#A simple test script to check if the AnkiConnect API is working correctly. It retrieves the names of all decks in Anki and prints them to the console.
+def main():
+    print("Hello. Revision!")
+
+    deck_names = ac.get_deck_names()
+    model_names = ac.get_model_names()
+    
+    # for deck_name in deck_names:
+    #    print(deck_name)
+    
+
+    for model_name in model_names:
+        print(model_name)
+
+    request = ac.build_deck_names_request()
+    #print("Request:", request)
+
+    response = ac.send_request(request)
+    #print(response)
+
+if __name__ == "__main__":
+    main()
